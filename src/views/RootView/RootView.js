e3ddb46603ad2c80abcf93f731473d6d388567a4
Nilan <nilan.marktanner@student.kit.edu>
2016-07-14 19:09:18 +0200

    validates project names on project creation

--- src/views/RootView/RootView.js
+++ src/views/RootView/RootView.js
@@ -3,6 +3,7 @@ import Relay from 'react-relay'
 import Tooltip from 'react-tooltip'
 import PureRenderMixin from 'react-addons-pure-render-mixin'
 import mapProps from 'map-props'
+import { validateProjectName } from 'utils/nameValidator'
 import ProjectSelection from 'components/ProjectSelection/ProjectSelection'
 import Header from 'components/Header/Header'
 import SideNav from 'views/RootView/SideNav'
@@ -188,7 +189,10 @@ export class RootView extends React.Component {
   }
 
   _addProject () {
-    const projectName = window.prompt('Project name')
+    var projectName = window.prompt('Project name:')
+    while (projectName != null && !validateProjectName(projectName)) {
+      projectName = window.prompt('The inserted project name was invalid. Enter a valid project name:')
+    }
     if (projectName) {
       Relay.Store.commitUpdate(new AddProjectMutation({
         projectName,
