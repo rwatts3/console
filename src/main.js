7542e195e35dd5740ad1779c05864f1a175ee328
Nilan <nilan.marktanner@student.kit.edu>
2016-07-15 19:11:16 +0200

    introduces redux for gettingStartedState

--- src/main.js
+++ src/main.js
@@ -5,6 +5,10 @@ import useRelay from 'react-router-relay'
 import { Router, browserHistory, applyRouterMiddleware } from 'react-router'
 import routes from './routes'
 import { updateNetworkLayer } from './utils/relay'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import { gettingStarted, updateGettingStartedState } from './reducers/gettingStarted'
+
 import loadAnalytics from './utils/analytics'
 
 import './utils/polyfils.ts'
@@ -17,12 +21,35 @@ browserHistory.listen(() => {
   analytics.page()
 })
 
+console.log(gettingStarted)
+
+const store = createStore(gettingStarted)
+var query = Relay.createQuery(Relay.QL`
+  query {
+    viewer {
+      user {
+        gettingStartedStatus
+      }
+    }
+  }`, {})
+Relay.Store.primeCache({query}, ({done, error}) => {
+  if (done) {
+    const data = Relay.Store.readQuery(query)[0]
+    console.log(data)
+    store.dispatch(updateGettingStartedState(data))
+  } else if (error) {
+    console.log(error)
+  }
+})
+
 ReactDOM.render((
-  <Router
-    forceFetch
-    routes={routes}
-    environment={Relay.Store}
-    render={applyRouterMiddleware(useRelay)}
-    history={browserHistory}
-  />
+  <Provider store={store}>
+    <Router
+      forceFetch
+      routes={routes}
+      environment={Relay.Store}
+      render={applyRouterMiddleware(useRelay)}
+      history={browserHistory}
+    />
+  </Provider>
 ), document.getElementById('root'))
