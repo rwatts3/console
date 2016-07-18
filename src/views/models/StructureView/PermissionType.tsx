2d72db4ea925f0d4339c0eb368fc793b4b0bb505
Johannes Schickling <schickling.j@gmail.com>
2016-07-18 10:59:06 +0200

    authenticated permissions

--- src/views/models/StructureView/PermissionType.tsx
+++ src/views/models/StructureView/PermissionType.tsx
@@ -4,14 +4,19 @@ const classes: any = require('./PermissionType.scss')
 
 interface Props {
   className?: string
-  dataCallback?: (data: any) => void
+  dataCallback?: (data: State) => void
   userType: UserType
-  userPath?: string
+  userPath: string
+  userRole: string
+  useUserRole: boolean
+  availableUserRoles: string[]
 }
 
 interface State {
   userType: UserType
-  userPath?: string
+  userPath: string
+  userRole: string
+  useUserRole: boolean
 }
 
 export default class PermissionType extends React.Component<Props, State> {
@@ -22,22 +27,56 @@ export default class PermissionType extends React.Component<Props, State> {
     this.state = {
       userType: props.userType,
       userPath: props.userPath,
+      userRole: props.userRole,
+      useUserRole: props.useUserRole,
     }
   }
 
-  _onChangeUserType (userType: UserType) {
-    this.setState({ userType }, () => {
-      this.props.dataCallback(this.state)
+  componentWillReceiveProps (props) {
+    this.setState({
+      userType: props.userType,
+      userPath: props.userPath,
+      userRole: props.userRole,
+      useUserRole: props.useUserRole,
     })
   }
 
+  _onChangeUserType (userType: UserType) {
+    this.setState({ userType } as State, this._dataCallback)
+  }
+
+  _onChangeUserRole (userRole: string) {
+    this.setState({ userRole, useUserRole: true } as State, this._dataCallback)
+  }
+
+  _toggleUseUserRole () {
+    const useUserRole = !this.state.useUserRole
+    const userRole = useUserRole && !this.state.userRole ? this.props.availableUserRoles[0] : this.state.userRole
+    this.setState({ useUserRole, userRole } as State, this._dataCallback)
+  }
+
+  _dataCallback () {
+    this.props.dataCallback(this.state)
+  }
+
   _renderAuthenticated () {
+    const emptyRoleToken = {}
     return (
-      <div>
-        <input type='checkbox' />
-        Limit to
-        <select>
-          <option value='GUEST'>Admin</option>
+      <div className={`${classes.authenticated} ${this.state.useUserRole ? '' : classes.inactive}`}>
+        <input
+          checked={this.state.useUserRole}
+          onChange={() => this._toggleUseUserRole()}
+          type='checkbox'
+        />
+        <span onClick={() => this._toggleUseUserRole()}>Limit to</span>
+        <select
+          value={this.state.userRole || emptyRoleToken}
+          onChange={(e) => this._onChangeUserRole((e.target as HTMLInputElement).value)}
+        >
+          <option disabled value={emptyRoleToken}>Select Role...</option>
+          {this.props.availableUserRoles.map((userRole) => (
+            <option key={userRole} value={userRole}>{userRole}</option>
+          ))}
         </select>
       </div>
     )
@@ -56,6 +95,7 @@ export default class PermissionType extends React.Component<Props, State> {
       <div className={`${classes.root} ${this.props.className}`}>
         <div className={classes.container}>
           <select
+            className={classes.userType}
             onChange={(e) => this._onChangeUserType((e.target as HTMLInputElement).value as UserType)}
             value={this.state.userType}
           >
