6e80527782cb8ebd8fc3b674572fef31326bd527
Rene Brandel <renebrandel@outlook.com>
2016-08-06 13:03:02 +0200

    DateTimeCell uses Date datatype

--- src/views/models/BrowserView/Cell/DateTimeCell.tsx
+++ src/views/models/BrowserView/Cell/DateTimeCell.tsx
@@ -4,12 +4,12 @@ import { CellProps } from '../../../../types/cells'
 
 const classes: any = require('../Cell.scss')
 
-export default class DateTimeCell extends React.Component<CellProps<string>,{}> {
+export default class DateTimeCell extends React.Component<CellProps<Date>,{}> {
   render() {
     return (
       <Datepicker
         className={classes.datepicker}
-        defaultValue={new Date(this.props.value)}
+        defaultValue={this.props.value}
         onChange={(m) => this.props.save(m.toISOString())}
         onCancel={() => this.props.cancel()}
         defaultOpen={true}
