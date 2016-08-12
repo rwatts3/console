4eba646c1dbd91cbb8d08472f4eca8420acd7a23
Rene Brandel <renebrandel@outlook.com>
2016-08-12 20:03:06 +0200

    Fixed not refreshing after delete bug!

--- src/mutations/DeleteRelationMutation.ts
+++ src/mutations/DeleteRelationMutation.ts
@@ -4,6 +4,8 @@ import {Project} from '../types/types'
 interface Props {
   relationId: string
   projectId: string
+  leftModelId: string
+  rightModelId: string
 }
 
 interface DeleteRelationPayload {
@@ -26,23 +28,35 @@ export default class DeleteRelationMutation extends Relay.Mutation<Props, {}> {
         `
     }
 
-  getConfigs () {
+  getConfigs() {
     return [{
       type: 'NODE_DELETE',
       parentName: 'project',
       parentID: this.props.projectId,
       connectionName: 'relations',
       deletedIDFieldName: 'deletedId',
+    }, {
+      type: 'NODE_DELETE',
+      parentName: 'model',
+      parentID: this.props.leftModelId,
+      connectionName: 'fields',
+      deletedIDFieldName: 'deletedId',
+    }, {
+      type: 'NODE_DELETE',
+      parentName: 'model',
+      parentID: this.props.rightModelId,
+      connectionName: 'fields',
+      deletedIDFieldName: 'deletedId',
     }]
   }
 
-  getVariables () {
+  getVariables() {
     return {
       relationId: this.props.relationId,
     }
   }
 
-  getOptimisticResponse () {
+  getOptimisticResponse() {
     return {
       deletedId: this.props.relationId,
     }
