b8b1adae3093e6f619b1646efe3ddf23e162f1a9
Rene Brandel <renebrandel@outlook.com>
2016-09-02 14:14:44 +0200

    Made the ProjectRootView more React-compliant

--- src/views/account/AccountView/SettingsTab.tsx
+++ src/views/account/AccountView/SettingsTab.tsx
@@ -42,69 +42,6 @@ class SettingsTab extends React.Component<Props, State> {
     }
   }
 
-  _saveChanges = () => {
-    const nameWasChanged = this.props.viewer.user.name !== this.state.name
-    const emailWasChanged = this.props.viewer.user.email !== this.state.email
-    const passwordWasChanged = this.state.newPasswordOne !== '' && this.state.newPasswordTwo !== ''
-
-    if (!nameWasChanged && !emailWasChanged && !passwordWasChanged) {
-      this.context.showNotification('No changes to save...', 'info')
-    }
-
-    if (nameWasChanged || emailWasChanged) {
-      this._handleUserChange()
-    }
-
-    if (passwordWasChanged) {
-      this._handlePasswordChange()
-    }
-  }
-
-  _handleUserChange () {
-    Relay.Store.commitUpdate(
-      new UpdateUserMutation({
-        userId: this.props.viewer.user.id,
-        email: this.state.email,
-        name: this.state.name,
-      }),
-      {
-        onSuccess: () => {
-          this.context.showNotification('Changes to email and name were saved.', 'success')
-        },
-        onFailure: (transaction) => {
-          onFailureShowNotification(transaction, this.context.showNotification)
-        },
-      }
-    )
-  }
-
-  _handlePasswordChange () {
-    if (this.state.newPasswordOne !== '' && this.state.newPasswordOne === this.state.newPasswordTwo) {
-      Relay.Store.commitUpdate(
-        new UpdatePasswordMutation({
-          userId: this.props.viewer.user.id,
-          oldPassword: this.state.oldPassword,
-          newPassword: this.state.newPasswordOne,
-        }),
-        {
-          onSuccess: () => {
-            this.context.showNotification('Changes to password successful.', 'success')
-            this.setState({
-              oldPassword: '',
-              newPasswordOne: '',
-              newPasswordTwo: '',
-            } as State)
-          },
-          onFailure: (transaction) => {
-            onFailureShowNotification(transaction, this.context.showNotification)
-          },
-        }
-      )
-    } else {
-      this.context.showNotification('Please enter the same new password twice.', 'error')
-    }
-  }
-
   render () {
     return (
       <div className={classes.root}>
@@ -158,11 +95,74 @@ class SettingsTab extends React.Component<Props, State> {
             onChange={(e: any) => this.setState({ newPasswordTwo: e.target.value } as State)}
           />
         </div>
-        <div className={classes.saveChanges} onClick={this._saveChanges}>
+        <div className={classes.saveChanges} onClick={this.saveChanges}>
           Save changes
         </div>
       </div>
     )
+  }
+
+  private saveChanges = () => {
+    const nameWasChanged = this.props.viewer.user.name !== this.state.name
+    const emailWasChanged = this.props.viewer.user.email !== this.state.email
+    const passwordWasChanged = this.state.newPasswordOne !== '' && this.state.newPasswordTwo !== ''
+
+    if (!nameWasChanged && !emailWasChanged && !passwordWasChanged) {
+      this.context.showNotification('No changes to save...', 'info')
+    }
+
+    if (nameWasChanged || emailWasChanged) {
+      this.handleUserChange()
+    }
+
+    if (passwordWasChanged) {
+      this.handlePasswordChange()
+    }
+  }
+
+  private handleUserChange () {
+    Relay.Store.commitUpdate(
+      new UpdateUserMutation({
+        userId: this.props.viewer.user.id,
+        email: this.state.email,
+        name: this.state.name,
+      }),
+      {
+        onSuccess: () => {
+          this.context.showNotification('Changes to email and name were saved.', 'success')
+        },
+        onFailure: (transaction) => {
+          onFailureShowNotification(transaction, this.context.showNotification)
+        },
+      }
+    )
+  }
+
+  private handlePasswordChange () {
+    if (this.state.newPasswordOne !== '' && this.state.newPasswordOne === this.state.newPasswordTwo) {
+      Relay.Store.commitUpdate(
+        new UpdatePasswordMutation({
+          userId: this.props.viewer.user.id,
+          oldPassword: this.state.oldPassword,
+          newPassword: this.state.newPasswordOne,
+        }),
+        {
+          onSuccess: () => {
+            this.context.showNotification('Changes to password successful.', 'success')
+            this.setState({
+              oldPassword: '',
+              newPasswordOne: '',
+              newPasswordTwo: '',
+            } as State)
+          },
+          onFailure: (transaction) => {
+            onFailureShowNotification(transaction, this.context.showNotification)
+          },
+        }
+      )
+    } else {
+      this.context.showNotification('Please enter the same new password twice.', 'error')
+    }
   }
 }
 
