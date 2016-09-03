54957a7c8f27387011a1b991bdbc459114888d3c
Rene Brandel <renebrandel@outlook.com>
2016-09-03 14:09:21 +0200

    Auto generation of field names

--- src/components/RelationExplanation/RelationExplanation.tsx
+++ src/components/RelationExplanation/RelationExplanation.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import * as Relay from 'react-relay'
-import {Model, Project} from '../../types/types'
+import {getModelName, getModelNamePlural} from '../../utils/namegetter'
+import {Project} from '../../types/types'
 const classes: any = require('./RelationExplanation.scss')
 
 interface Props {
@@ -17,55 +18,54 @@ class RelationExplanation extends React.Component<Props, {}> {
 
   render() {
     const {fieldOnLeftModelIsList, leftModelId, fieldOnRightModelIsList, rightModelId} = this.props
-
+    const models = this.props.project.models.edges.map((edge) => edge.node)
     const leftModelMultiplicity = fieldOnLeftModelIsList ? 'Many' : 'One'
     const leftModelName = fieldOnRightModelIsList
-      ? this.getModelNamePlural(leftModelId) : this.getModelName(leftModelId)
-    const leftNameS = this.getModelName(leftModelId).slice(-1) === 's'
+      ? getModelNamePlural(leftModelId, models) : getModelName(leftModelId, models)
+    const leftNameS = getModelName(leftModelId, models).slice(-1) === 's'
       ? `'` : `'s`
 
     const rightModelMultiplicity = fieldOnRightModelIsList ? 'Many' : 'One'
     const rightModelName = fieldOnLeftModelIsList
-      ? this.getModelNamePlural(rightModelId) : this.getModelName(rightModelId)
-    const rightNameS = this.getModelName(rightModelId).slice(-1) === 's' ? `'` : `'s`
+      ? getModelNamePlural(rightModelId, models) : getModelName(rightModelId, models)
+    const rightNameS = getModelName(rightModelId, models).slice(-1) === 's' ? `'` : `'s`
     return (
       <div className={classes.root}>
         <div className={classes.sentence}>
-          <span className={classes.multiplicity}>{rightModelMultiplicity}</span> <span className={classes.model}>{leftModelName}</span>
+          <span className={classes.multiplicity}>{rightModelMultiplicity}</span>
+          {' '}
+          <span className={classes.model}>{leftModelName}</span>
           {` ${rightModelMultiplicity === 'One' ? 'is' : 'are'} related to `}
-          <span className={classes.multiplicity}>{leftModelMultiplicity.toLowerCase()}</span> <span className={classes.model}>{rightModelName}</span>
+          <span className={classes.multiplicity}>{leftModelMultiplicity.toLowerCase()}</span>
+          {' '}
+          <span className={classes.model}>{rightModelName}</span>
         </div>
         {this.props.fieldOnLeftModelName &&
         <div className={classes.sentence}>
-          <span className={classes.model}>{this.getModelName(leftModelId)}</span>{`${leftNameS} field `}
+          <span className={classes.model}>{getModelName(leftModelId, models)}</span>
+          {`${leftNameS} field `}
           <span className={classes.field}>{this.props.fieldOnLeftModelName}</span>
           {` represents `}
-          <span className={classes.multiplicity}>{leftModelMultiplicity.toLowerCase()}</span> <span className={classes.model}>{rightModelName}</span>
+          <span className={classes.multiplicity}>{leftModelMultiplicity.toLowerCase()}</span>
+          {' '}
+          <span className={classes.model}>{rightModelName}</span>
         </div>
         }
         {this.props.fieldOnRightModelName &&
         <div className={classes.sentence}>
-          <span className={classes.model}>{this.getModelName(rightModelId)}</span>{`${rightNameS} field `}
+          <span className={classes.model}>{getModelName(rightModelId, models)}</span>
+          {`${rightNameS} field `}
           <span className={classes.field}>{this.props.fieldOnRightModelName}</span>
           {` represents `}
-          <span className={classes.multiplicity}>{rightModelMultiplicity.toLowerCase()}</span> <span className={classes.model}>{leftModelName}</span>
+          <span className={classes.multiplicity}>{rightModelMultiplicity.toLowerCase()}</span>
+          {' '}
+          <span className={classes.model}>{leftModelName}</span>
         </div>
         }
       </div>
     )
   }
 
-  private getModelName = (id: string): string => {
-    return this.getModel(id).name
-  }
-
-  private getModelNamePlural = (id: string): string => {
-    return this.getModel(id).namePlural
-  }
-
-  private getModel = (id: string): Model => {
-    return this.props.project.models.edges.map((edge) => edge.node).find((node) => node.id === id)
-  }
 }
 
 export default Relay.createContainer(RelationExplanation, {
