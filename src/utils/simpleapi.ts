84fc3b39875253f8e4c9e55b025dae533a55e44c
Rene Brandel <renebrandel@outlook.com>
2016-09-04 18:00:21 +0200

    Removed unnecessary code from the simple api

--- src/utils/simpleapi.ts
+++ src/utils/simpleapi.ts
@@ -1,8 +1,6 @@
-import {toGQL} from '../views/models/utils'
 import {Field, OrderBy} from '../types/types'
 import * as Immutable from 'immutable'
-import {TypedValue} from '../types/utils'
-import {isScalar, isNonScalarList} from './graphql'
+import {isScalar} from './graphql'
 import * as cookiestore from './cookiestore'
 import { Lokka } from 'lokka'
 import { Transport } from 'lokka-transport-http'
@@ -13,54 +11,6 @@ export function getLokka(projectId: string): any {
   const headers = { Authorization: `Bearer ${token}` }
   const transport = new Transport(clientEndpoint, { headers })
   return new Lokka({transport})
-}
-
-export function addNode(lokka: any, modelName: string, fieldValues: { [key: string]: any }): Promise<any> {
-  const inputString = fieldValues
-    .mapToArray((fieldName, obj) => obj)
-    .filter(({value}) => value !== null)
-    .filter(({field}) => (!isNonScalarList(field)))
-    .map(({field, value}) => toGQL(value, field))
-    .join(' ')
-
-  const inputArgumentsString = inputString.length > 0 ? `(${inputString})` : ''
-
-  const mutation = `
-    {
-      create${modelName}${inputArgumentsString} {
-        id
-      }
-    }
-  `
-  return lokka.mutate(mutation)
-}
-
-export function updateNode(lokka: any, modelName: string, value: TypedValue,
-                           field: Field, nodeId: string): Promise<any> {
-  const mutation = `
-    {
-      update${modelName}(
-        id: "${nodeId}"
-        ${toGQL(value, field)}
-      ) {
-        id
-      }
-    }
-  `
-  return lokka.mutate(mutation)
-}
-
-export function deleteNode(lokka: any, modelName: string, nodeId: string): Promise<any> {
-  const mutation = `
-    {
-      delete${modelName}(
-        id: "${nodeId}"
-      ) {
-        id
-      }
-    }
-  `
-  return lokka.mutate(mutation)
 }
 
 export function queryNodes(lokka: any, modelNamePlural: string, fields: Field[], skip: number = 0, first: number = 50,
