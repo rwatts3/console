23eb5bcee2cd28b9630d80ed2e6c962e3760616a
Johannes Schickling <schickling.j@gmail.com>
2016-09-15 19:36:15 +0200

    added customer id

--- src/views/ProjectRootView/TokenRedirectView.tsx
+++ src/views/ProjectRootView/TokenRedirectView.tsx
@@ -7,9 +7,11 @@ export default class TokenRedirectView extends React.Component<{}, {}> {
 
   componentWillMount () {
     const token = getQueryVariable('token')
+    const customerId = getQueryVariable('customer_id')
     if (token) {
       window.localStorage.clear()
       cookiestore.set('graphcool_auth_token', token)
+      cookiestore.set('graphcool_customer_id', customerId)
       updateNetworkLayer()
       window.location.href = window.location.origin
     }
