ab03ad6e5e70807b6be4742e1bb4fbbc5adb49c9
Rene Brandel <renebrandel@outlook.com>
2016-09-22 18:34:14 +0200

    Basic layout of 1st Getting Started step done

--- src/views/GettingStartedView/OnboardingView.tsx
+++ src/views/GettingStartedView/OnboardingView.tsx
@@ -20,33 +20,40 @@ class OnboardingView extends React.Component<Props, {}> {
 
   render() {
     return (
+      <div className='flex justify-center items-center h-100 w-100 bg-black-50'>
+        <div className='pa4 bg-white br3 flex'>
+          <div className='w-80'>
+          <div>🙌</div>
           <div>
-            <div>🙌</div>
-            <div>
-              <h2><strong>Hi {this.props.firstName}</strong>, welcome to our Dashboard.</h2>
-              <p>
-                To make your start a bit easier, we have prepared a little tour for you.
+            <h2><strong>Hi {this.props.firstName}</strong>, welcome to our Dashboard.</h2>
+            <p>
+              To make your start a bit easier, we have prepared a little tour for you.
               </p>
-              <p>
-                It will take about 3 minutes and show you the basic features<br />
-                by creating a GraphQL backend for an Instagram clone.
+            <p>
+              It will take about 3 minutes and show you the basic features<br />
+              by creating a GraphQL backend for an Instagram clone.
               </p>
-            </div>
-            <div>
-              <div
-                style={{width: 260}}
-                onClick={this.getStarted}
-              >
-                Let’s go
-              </div>
-              <div
-                style={{width: 170}}
-                onClick={this.skipGettingStarted}
-              >
-                Skip tour
-              </div>
-            </div>
           </div>
+          <div className='w-100 flex justify-center flex-column items-center'>
+            <div
+              className='br2 ba b--green f3 green tc pa2 pointer ttu dim'
+              onClick={this.getStarted}
+              >
+              Get Started
+            </div>
+            <div
+              className='mt3 underline pointer dim'
+              onClick={this.skipGettingStarted}
+            >
+              Skip tour
+              </div>
+          </div>
+          </div>
+          <div className='w-20 bg-black-10 w-100'>
+            placeholder
+          </div>
+        </div>
+      </div>
     )
   }
 
