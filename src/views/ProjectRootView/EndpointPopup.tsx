812265f09774195dc5b8b1dc42174814092812c3
Julian Bauer <julian@overnice.de>
2016-10-16 18:08:06 +0200

    endpoints

--- src/views/ProjectRootView/EndpointPopup.tsx
+++ src/views/ProjectRootView/EndpointPopup.tsx
@@ -3,6 +3,9 @@ import {connect} from 'react-redux'
 import {bindActionCreators} from 'redux'
 import {ReduxAction} from '../../types/reducers'
 import {closePopup} from '../../actions/popup'
+import styled from 'styled-components'
+import {particles, variables, Icon} from 'graphcool-styles'
+import * as cx from 'classnames'
 
 interface Props {
   id: string
@@ -15,11 +18,78 @@ interface State {
 class EndpointPopup extends React.Component<Props, State> {
 
   render() {
+
+    const activeEndpointType = `
+      background: ${variables.blue};
+      color: ${variables.white}
+    `
+
+    const EndpointType = styled.div`
+      background: ${variables.gray07};
+      padding: ${variables.size16};
+      color: ${variables.gray30};
+      letter-spacing: 1px;
+      border-radius: 2px;
+      
+     ${props => props.active && activeEndpointType}
+    `
+
     return (
-      <div className='flex bg-black-50 w-100 h-100 justify-center items-center'>
-        <div className='pa2 bg-white' style={{pointerEvents: 'all'}}>
-          List endpoints
-          <div onClick={() => this.props.closePopup(this.props.id)}>Close</div>
+      <div className={cx(
+        particles.flex,
+        particles.bgBlack50,
+        particles.w100,
+        particles.h100,
+        particles.justifyCenter,
+        particles.itemsCenter,
+      )}>
+        <div className={cx(particles.bgWhite, particles.br2)} style={{pointerEvents: 'all'}}>
+
+          <div className={cx(particles.pa25, particles.flex, particles.itemsCenter)}>
+            <div className={cx(
+              particles.f14,
+              particles.fw6,
+              particles.ttu,
+              particles.flex,
+              particles.itemsCenter,
+            )}>
+              <EndpointType>Relay</EndpointType>
+              <EndpointType active >Simple</EndpointType>
+            </div>
+            <div className={cx(
+              particles.ph25,
+              particles.f25,
+              particles.fw3,
+
+            )}>
+              https://api.graph.cool/simple/v1/cim2556e300e20plm8aj7e4wo
+            </div>
+
+          </div>
+
+          <div className={cx(
+            particles.flex,
+            particles.justifyEnd,
+            particles.bt,
+            particles.bBlack20,
+            particles.pa25,
+          )}>
+            <div
+              className={cx(
+              particles.pa16,
+              particles.bgBlue,
+              particles.br2,
+              particles.white,
+              particles.f16,
+              particles.pointer
+
+            )}
+              onClick={() => this.props.closePopup(this.props.id)}
+            >
+              Close
+            </div>
+          </div>
+
         </div>
       </div>
     )
