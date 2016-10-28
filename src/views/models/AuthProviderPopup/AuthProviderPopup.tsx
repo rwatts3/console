d6c7ee3498beea1ad07a26351cf734b9c8eaad9e
Johannes Schickling <schickling.j@gmail.com>
2016-10-28 12:54:46 +0200

    Auth provider popup lacks padding - closes #311

--- src/views/models/AuthProviderPopup/AuthProviderPopup.tsx
+++ src/views/models/AuthProviderPopup/AuthProviderPopup.tsx
@@ -2,7 +2,9 @@ import * as React from 'react'
 import * as Relay from 'react-relay'
 import AuthProviderSidePanel from './AuthProviderSidePanel'
 import Icon from '../../../components/Icon/Icon'
-import {Project, AuthProviderType} from '../../../types/types'
+import { $p } from 'graphcool-styles'
+import * as cx from 'classnames'
+import { Project, AuthProviderType } from '../../../types/types'
 
 interface Props {
   project: Project
@@ -22,13 +24,16 @@ class AuthProviderPopup extends React.Component<Props, State> {
   render() {
     const authProviders = this.props.project.authProviders.edges.map(edge => edge.node)
     return (
-      <div className='flex justify-center items-center h-100 w-100 bg-white-50' style={{ pointerEvents: 'all' }}>
-        <div className='bg-white br-2 shadow-2' style={{ width: 900 }}>
-          <div className='bg-accent flex justify-between items-center white pa4'>
-            <div className='f-38 fw1'>
+      <div
+        className={cx($p.flex, $p.justifyCenter, $p.itemsCenter, $p.h100, $p.w100, $p.bgWhite50)}
+        style={{ pointerEvents: 'all' }}
+      >
+        <div className={cx($p.bgWhite, $p.br2, $p.buttonShadow)} style={{ width: 900 }}>
+          <div className={cx($p.bgGreen, $p.flex, $p.justifyBetween, $p.itemsCenter, $p.white, $p.pa25)}>
+            <div className={cx($p.f38, $p.fw3)}>
               Authentication Methods
             </div>
-            <div className='pointer' onClick={this.props.close}>
+            <div className={$p.pointer} onClick={this.props.close}>
               <Icon
                 src={require('assets/icons/close.svg')}
                 width={40}
@@ -37,66 +42,57 @@ class AuthProviderPopup extends React.Component<Props, State> {
               />
             </div>
           </div>
-          <div className='flex justify-between'>
-            <div className='flex flex-column br b--black-10' style={{ flex: '0 0 270px' }}>
+          <div className={cx($p.flex, $p.justifyBetween)}>
+            <div className={cx($p.flex, $p.flexColumn, $p.br, $p.bBlack10)} style={{ flex: '0 0 270px' }}>
               <div
-                className={`
-                flex pa4 bb b--black-10 items-center pointer justify-between
-                ${this.state.selectedType === 'AUTH_PROVIDER_EMAIL' ? 'bg-black-05' : ''}
-                `}
+                className={cx(
+                  $p.flex, $p.pa25, $p.bb, $p.bBlack10, $p.itemsCenter, $p.pointer, $p.justifyBetween,
+                  this.state.selectedType === 'AUTH_PROVIDER_EMAIL' && $p.bgBlack04
+                )}
                 onClick={() => this.setState({ selectedType: 'AUTH_PROVIDER_EMAIL' })}
               >
-                <div className='flex items-center'>
+                <div className={cx($p.flex, $p.itemsCenter)}>
                   <Icon
                     src={require('assets/icons/logo.svg')}
                     width={40}
                     height={40}
                     color='#00B861'
                   />
-                  <div className='fw1 f-25 ml-16'>
+                  <div className={cx($p.fw3, $p.f25, $p.ml16)}>
                     Email
                   </div>
                 </div>
                 <div>
                   {authProviders.find(a => a.type === 'AUTH_PROVIDER_EMAIL' && a.isEnabled) &&
-                  <Icon
-                    src={require('assets/new_icons/check.svg')}
-                    color='#7ED321'
-                  />
+                  <Icon src={require('assets/new_icons/check.svg')} color='#7ED321'/>
                   }
                 </div>
               </div>
               <div
-                className={`
-                flex pa4 bb b--black-10 items-center pointer justify-between
-                ${this.state.selectedType === 'AUTH_PROVIDER_DIGITS' ? 'bg-black-05' : ''}
-                `}
+                className={cx(
+                  $p.flex, $p.pa25, $p.bb, $p.bBlack10, $p.itemsCenter, $p.pointer, $p.justifyBetween,
+                  this.state.selectedType === 'AUTH_PROVIDER_DIGITS' && $p.bgBlack04
+                )}
                 onClick={() => this.setState({ selectedType: 'AUTH_PROVIDER_DIGITS' })}
               >
                 <img src={require('assets/graphics/digits.png')}/>
                 <div>
                   {authProviders.find(a => a.type === 'AUTH_PROVIDER_DIGITS' && a.isEnabled) &&
-                  <Icon
-                    src={require('assets/new_icons/check.svg')}
-                    color='#7ED321'
-                  />
+                  <Icon src={require('assets/new_icons/check.svg')} color='#7ED321'/>
                   }
                 </div>
               </div>
               <div
-                className={`
-                flex pa4 bb b--black-10 items-center pointer justify-between
-                ${this.state.selectedType === 'AUTH_PROVIDER_AUTH0' ? 'bg-black-05' : ''}
-                `}
+                className={cx(
+                  $p.flex, $p.pa25, $p.bb, $p.bBlack10, $p.itemsCenter, $p.pointer, $p.justifyBetween,
+                  this.state.selectedType === 'AUTH_PROVIDER_AUTH0' && $p.bgBlack04
+                )}
                 onClick={() => this.setState({ selectedType: 'AUTH_PROVIDER_AUTH0' })}
               >
                 <img src={require('assets/graphics/auth0.png')}/>
                 <div>
                   {authProviders.find(a => a.type === 'AUTH_PROVIDER_AUTH0' && a.isEnabled) &&
-                  <Icon
-                    src={require('assets/new_icons/check.svg')}
-                    color='#7ED321'
-                  />
+                  <Icon src={require('assets/new_icons/check.svg')} color='#7ED321'/>
                   }
                 </div>
               </div>
