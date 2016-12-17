15fbae7c75bc04ab0b871f86782ffecccc52114c
Ben Kremer <ben.kremer@hotmail.co.uk>
2016-12-17 18:36:11 +0100

    Adds IntegrationsHeader component

---
+++ src/views/Integrations/IntegrationsHeader/IntegrationsHeader.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+
+const IntegrationsHeader = () => {
+  return (
+    <div>
+      <h1>Integrations</h1>
+      <h2>Integrate everything you need</h2>
+    </div>
+  )
+}
+
+export default IntegrationsHeader
