54ce746835731cc2bf30e9e07e7bd293a3bfd865
Ben Kremer <ben.kremer@hotmail.co.uk>
2016-12-17 18:37:35 +0100

    Adds IntegrationsHeader component to render

--- src/views/Integrations/IntegrationsView.tsx
+++ src/views/Integrations/IntegrationsView.tsx
@@ -2,30 +2,16 @@ import * as React from 'react'
 import * as Relay from 'react-relay'
 import Helmet from 'react-helmet'
 import IntegrationsCardGrid from './IntegrationsCardGrid/IntegrationsCardGrid'
+import IntegrationsHeader from './IntegrationsHeader/IntegrationsHeader'
 
 export default class IntegrationsView extends React.Component<{}, {}> {
-
   render() {
     return (
       <div>
         <Helmet title='Integrations' />
+        <IntegrationsHeader />
         <IntegrationsCardGrid />
       </div>
     )
   }
-
 }
-
-/* 
-export default Relay.createContainer(IntegrationsView, {
-  fragments: {
-    viewer: () => Relay.QL`
-      fragment on Viewer {
-        project: projectByName(projectName: $projectName) {
-          ${PermissionsList.getFragment('project')}
-        }
-      }
-    `,
-  }
-}) 
-*/
