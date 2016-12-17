4a30c3d7cdca353d73ababe8dd07b30a355804bd
Tim Suchanek <tim.suchanek@gmail.com>
2016-12-17 18:28:39 +0100

    Fix #313

--- src/views/models/DatabrowserView/Cell/StringCell.tsx
+++ src/views/models/DatabrowserView/Cell/StringCell.tsx
@@ -12,10 +12,24 @@ declare module './cells' {
   }
 }
 
-export class StringCell extends React.Component<CellProps<string>, {}> {
+interface State {
+  value: string
+}
+
+export class StringCell extends React.Component<CellProps<string>, State> {
 
   refs: {
     input: HTMLInputElement,
+  }
+
+  enterPressed: boolean
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      value: props.value,
+    }
   }
 
   componentWillReceiveProps(nextProps) {
@@ -25,26 +39,66 @@ export class StringCell extends React.Component<CellProps<string>, {}> {
   }
 
   render() {
-    const numLines = this.props.value ? this.props.value.split(/\r\n|\r|\n/).length : 1
+    const numLines = this.state.value ? this.state.value.split(/\r\n|\r|\n/).length : 1
 
     return (
-      <textarea
-        autoFocus
-        type='text'
-        ref='input'
-        defaultValue={this.props.value}
-        onKeyDown={this.onKeyDown}
-        style={{
-          height: Math.min(Math.max(56, numLines * 20), 300),
-        }}
-        onBlur={(e: any) => this.props.save(stringToValue(e.target.value, this.props.field))}
-        onChange={this.onChange}
-      />
+      numLines > 1 ? (
+        <textarea
+          autoFocus
+          type='text'
+          ref='input'
+          value={this.state.value}
+          onKeyDown={this.onKeyDown}
+          style={{
+            height: Math.min(Math.max(56, numLines * 20), 300),
+          }}
+          onBlur={(e: any) => {
+            this.props.save(stringToValue(e.target.value, this.props.field))
+          }}
+          onChange={this.onChange}
+        />
+      ) : (
+        <input
+          autoFocus
+          type='text'
+          ref='input'
+          value={this.state.value}
+          onKeyDown={this.onKeyDown}
+          onBlur={(e: any) => {
+            if (this.enterPressed) {
+              this.stopEvent(e)
+              this.enterPressed = false
+              return
+            }
+            this.props.save(stringToValue(e.target.value, this.props.field))
+          }}
+          onChange={this.onChange}
+        />
+      )
     )
+  }
+
+  private stopEvent = (e: any) => {
+    e.preventDefault()
+    if (typeof e.stopImmediatePropagation === 'function') {
+      e.stopImmediatePropagation()
+    }
+    if (typeof e.stopPropagation === 'function') {
+      e.stopPropagation()
+    }
   }
 
   private onKeyDown = (e: any) => {
     // filter arrow keys
+    const numLines = e.target.value.split(/\r\n|\r|\n/).length
+    if (e.keyCode === 13) {
+      if (numLines === 1) {
+        this.setState({value: e.target.value + '\n'})
+        this.enterPressed = true
+        this.stopEvent(e)
+      }
+      return
+    }
     if ([37,38,39,40].includes(e.keyCode)) {
       return
     }
@@ -52,6 +106,10 @@ export class StringCell extends React.Component<CellProps<string>, {}> {
   }
 
   private onChange = (e: any) => {
+    const {value} = e.target
+    this.setState({value})
+
+    // ONLY FOR ONBOARDING
     if (typeof this.props.nextStep !== 'function') {
       return
     }
