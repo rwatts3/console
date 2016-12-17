284894ddf0b4758bb9d2436632009c1fc2f48fdc
Ben Kremer <ben.kremer@hotmail.co.uk>
2016-12-17 19:21:46 +0100

    Fixes no-scroll issue due to 'overflow: hidden'

--- src/views/Integrations/IntegrationsView.tsx
+++ src/views/Integrations/IntegrationsView.tsx
@@ -3,11 +3,13 @@ import * as Relay from 'react-relay'
 import Helmet from 'react-helmet'
 import IntegrationsCardGrid from './IntegrationsCardGrid/IntegrationsCardGrid'
 import IntegrationsHeader from './IntegrationsHeader/IntegrationsHeader'
+import {$p} from 'graphcool-styles'
+import * as cx from 'classnames'
 
 export default class IntegrationsView extends React.Component<{}, {}> {
   render() {
     return (
-      <div>
+      <div className={cx($p.overflowScroll, $p.h100, $p.bgBlack04)}>
         <Helmet title='Integrations' />
         <IntegrationsHeader />
         <IntegrationsCardGrid />
