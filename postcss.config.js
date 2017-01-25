ce3d2e4042f8c953a3f72e48170c9e7bdc609610
Tim Suchanek <tim.suchanek@gmail.com>
2017-01-25 11:15:31 +0100

    push

---
+++ postcss.config.js
@@ -0,0 +1,12 @@
+module.exports = () => ({
+  plugins: {
+    'postcss-simple-vars': {
+      variables: () => require('graphcool-styles/dist/variables/variables.js'),
+    },
+    'postcss-inject': {
+      cssFilePath: 'node_modules/graphcool-styles/dist/styles.css'
+    },
+    'postcss-cssnext': {},
+    'postcss-inherit': {},
+  }
+})
