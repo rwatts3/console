9a34acdeb57b0e3a4a4cb58e9f92bdbbd666815b
Nikolas Burk <nikolas.burk@gmail.com>
2017-01-27 14:56:15 +0100

    tried to implement copy functionality for authentication but icon didn't render

--- src/views/Settings/Authentication/TokenRow.tsx
+++ src/views/Settings/Authentication/TokenRow.tsx
@@ -2,14 +2,23 @@ import * as React from 'react'
 import {PermanentAuthToken} from '../../../types/types'
 import Icon from 'graphcool-styles/dist/components/Icon/Icon'
 import {$p} from 'graphcool-styles'
-// import CopyToClipboard from 'react-copy-to-clipboard'
+import CopyToClipboard from 'react-copy-to-clipboard'
+import * as cx from 'classnames'
 
 interface Props {
   permanentAuthToken: PermanentAuthToken
   deleteSystemToken: (PermanentAuthToken) => void
 }
 
-export default class TokenRow extends React.Component<Props, {}> {
+interface State {
+  copied: boolean
+}
+
+export default class TokenRow extends React.Component<Props, State> {
+
+  state = {
+    copied: false,
+  }
 
   constructor(props) {
     super(props)
@@ -53,11 +62,24 @@ export default class TokenRow extends React.Component<Props, {}> {
             onClick={() => this.props.deleteSystemToken(this.props.permanentAuthToken)}
           />
         </div>
-        <div
-          className='tokenRow'
-        >
-          {this.props.permanentAuthToken.token}
+        <div className='flex'>
+          <div className='relative f25 fw3 mt10 overflowXHidden nowrap'>
+            {this.props.permanentAuthToken.token}
+          </div>
+          {/*<CopyToClipboard*/}
+            {/*text={this.props.permanentAuthToken.token}*/}
+            {/*onCopy={() => this.setState({copied: true} as State)}*/}
+          {/*>*/}
+            {/*<Icon*/}
+              {/*className={cx($p.ml10, $p.pointer, $p.buttonShadow)}*/}
+              {/*color={'rgba(0,0,0,.5)'}*/}
+              {/*src={require('../../../assets/icons/copy.svg')}*/}
+              {/*width={34}*/}
+              {/*height={34}*/}
+            {/*/>*/}
+          {/*</CopyToClipboard>*/}
         </div>
+
       </div>
     )
   }
