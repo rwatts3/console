ba1b55fde2a4f0700d47e176fce05f4bf6451f7c
Nikolas Burk <nikolas.burk@gmail.com>
2017-01-31 16:27:18 +0100

    implemented HorizontalSelect

---
+++ src/views/RelationsPopup/FieldNameInput.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react'
+
+interface State {
+
+}
+
+interface Props {
+
+}
+
+export default class FieldNameInput extends React.Component<Props, State> {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
