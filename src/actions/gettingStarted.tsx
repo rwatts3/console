3cc73504606a1026e0fc920fed193728a1ac25bc
Johannes Schickling <schickling.j@gmail.com>
2017-02-03 11:02:03 +0100

    added getting started completd

--- src/actions/gettingStarted.tsx
+++ src/actions/gettingStarted.tsx
@@ -44,6 +44,7 @@ function updateReduxAndRelay(dispatch: (action: ReduxAction) => any,
           onboardingStatusId,
           gettingStarted,
           gettingStartedSkipped,
+          gettingStartedCompleted: gettingStarted === 'STEP6_DONE',
           gettingStartedExample,
         }),
       {
