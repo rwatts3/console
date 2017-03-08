b09f036b26e7d6638fdf39dd24ba4fbdf6cad360
Tim Suchanek <tim.suchanek@gmail.com>
2017-03-08 11:46:52 +0100

    Add OfflinePlugin

--- webpack.config.build.js
+++ webpack.config.build.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const cssnano = require('cssnano')
 const path = require('path')
 const config = require('./webpack.config')
+const OfflinePlugin = require('offline-plugin')
 
 module.exports = {
   entry: {
@@ -112,6 +113,7 @@ module.exports = {
         },
       }
     }),
+    new OfflinePlugin(),
   ],
   resolve: {
     modules: [path.resolve('./src'), 'node_modules'],
