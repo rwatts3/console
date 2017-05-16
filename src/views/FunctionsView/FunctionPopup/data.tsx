c0b40ebef0cac0030bfb51a4061adaddb5feaa37
Tim Suchanek <tim.suchanek@gmail.com>
2017-05-16 22:55:47 +0200

    Closes #962

--- src/views/FunctionsView/FunctionPopup/data.tsx
+++ src/views/FunctionsView/FunctionPopup/data.tsx
@@ -5,7 +5,7 @@ export type FunctionBinding = 'TRANSFORM_ARGUMENT' | 'PRE_WRITE' | 'TRANSFORM_PA
 const texts = {
   'TRANSFORM_ARGUMENT': `, you can manipulate your data before it get’s validated by our
     Constraints and Permissions APIs. Use this to transform credit card numbers, slugs etc.`,
-  'PRE_WRITE': `, you can validate your data before it gets written to the databse.
+  'PRE_WRITE': `, you can validate your data before it gets written to the database.
     Use this to perform a stripe transaction etc.
   `,
   'TRANSFORM_PAYLOAD': `, you can manipulate the data that will be sent back to the client.
