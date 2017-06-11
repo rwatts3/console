5645b29587c554be828b97bd0a98056b8aa15f83
Tim Suchanek <tim.suchanek@gmail.com>
2017-06-11 15:47:44 +0200

    add console source

--- src/views/AuthView/AuthView.tsx
+++ src/views/AuthView/AuthView.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import Auth0LockWrapper from '../../components/Auth0LockWrapper/Auth0LockWrapper'
-import { Response } from '../../mutations/AuthenticateCustomerMutation'
+import {Response} from '../../mutations/AuthenticateCustomerMutation'
+import * as cookiestore from 'cookiestore'
 
 interface Props {
   initialScreen: 'login' | 'signUp'
@@ -9,9 +10,27 @@ interface Props {
 export default class AuthView extends React.Component<Props, {}> {
 
   render() {
-    const successCallback = (response: Response) => {
+    const successCallback = async (response: Response) => {
       if ((new Date().getTime() - new Date(response.user.createdAt).getTime()) < 60000) {
         // this is a workaround instead of using the router to re-setup relay
+        await fetch(`${__BACKEND_ADDR__}/system`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${cookiestore.get('graphcool_auth_token')}`,
+          },
+          body: JSON.stringify({
+            query: `mutation {
+              updateCrmCustomerInformation(input: {
+                clientMutationId: "asd"
+                signupSource: CONSOLE
+              }) {
+                clientMutationId
+              }
+            }`,
+          }),
+        })
+
         window.location.pathname = '/after-signup'
       } else {
         // this is a workaround instead of using the router to re-setup relay
