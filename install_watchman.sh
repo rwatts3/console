0a58ad40b8e8a70cbbe109851ac3a4dc7da2b9ec
Tim Suchanek <tim.suchanek@gmail.com>
2017-07-27 18:00:11 +0200

    add pkg-config

--- install_watchman.sh
+++ install_watchman.sh
@@ -2,7 +2,7 @@ set -x
 set -e
 git clone https://github.com/facebook/watchman.git
 cd watchman
-sudo apt-get update; sudo apt-get install -y autoconf automake build-essential python-dev
+sudo apt-get update; sudo apt-get install -y autoconf automake build-essential python-dev pkg-config
 ./autogen.sh
 ./configure
 make
