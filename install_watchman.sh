510b833b10e25c2f0441113cc12cee8b725231f8
Tim Suchanek <tim.suchanek@gmail.com>
2017-07-27 17:54:09 +0200

    add watchman

---
+++ install_watchman.sh
@@ -0,0 +1,9 @@
+set -x
+set -e
+git clone https://github.com/facebook/watchman.git
+cd watchman
+sudo apt-get update; sudo apt-get install -y autoconf automake build-essential python-dev
+./autogen.sh
+./configure
+make
+sudo make install
