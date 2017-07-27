1489a0a07f9c9f4117ec4e66dc55773f2575d439
Tim Suchanek <tim.suchanek@gmail.com>
2017-07-27 18:31:10 +0200

    fix watchman problem

--- install_watchman.sh
+++ install_watchman.sh
@@ -1,5 +1,9 @@
 set -x
+uname -a
+
 set -e
+PATH=$PWD:$PATH
+
 git clone https://github.com/facebook/watchman.git
 cd watchman
 sudo apt-get update; sudo apt-get install -y autoconf automake build-essential python-dev pkg-config
