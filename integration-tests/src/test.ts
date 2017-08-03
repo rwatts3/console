e3b254084efef1674c2bb8fd656f2f980cbb61c6
Tim Suchanek <tim.suchanek@gmail.com>
2017-08-03 10:58:58 +0200

    wip tests

--- integration-tests/src/test.ts
+++ integration-tests/src/test.ts
@@ -13,20 +13,10 @@ import {deleteCustomer, extractCustomerInfo} from './deleteUser'
 let cookies: any[] = []
 
 test.before('signup, get cookie and run onboarding', async t => {
-  let worked = false
-  let count = 0
-  while (!worked && count < 5) {
-    try {
-      console.log('######### signup')
-      cookies = await signup()
-      console.log('######### onboarding')
-      await onboarding(cookies)
-      worked = false
-      count++
-    } catch (e) {
-      //
-    }
-  }
+  console.log('######### signup')
+  cookies = await signup()
+  console.log('######### onboarding')
+  await onboarding(cookies)
 })
 
 test.after('delete user', async () => {
